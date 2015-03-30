4bccaf17c37726d31de3380049cefe7cb20fc2f2
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-03-30 09:54:41 +0200

    test2

--- oculus/src/teamd/hello/HelloWorld.java
+++ oculus/src/teamd/hello/HelloWorld.java
@@ -31,7 +31,7 @@ public class HelloWorld {
 		
 		System.out.println("Caros TEST");
 		
-		System.out.println("Hey");
+		System.out.println("Hi there!");
 		
 		//TODO Write Java 8 Tests
 		// Really?

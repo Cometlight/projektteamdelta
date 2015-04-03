1b91fc85143d3101e26672d676e4bc998f986e9c
Andrew Sparr <andrew.sparr@students.fhv.at>
2015-04-03 12:39:13 +0200

    Revert "Revert "Add all packages that represent the layers.""
    This reverts commit 47237f419a422f9f7de4eb8e4636c9b62ccea05b.

---
+++ oculus/src/at/itb13/oculus/application/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all classes that are responsible for the workflow of the Use-Cases.
+ * 
+ * @author Daniel Scheffknecht
+ * @date 01.04.2015
+ */
+package at.itb13.oculus.application;

455dab892b16630da6534f71671192259a1522d6
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-04-07 17:45:11 +0200

    Finalize EventtypeDao

--- oculus/src/at/itb13/oculus/technicalServices/dao/EventtypeDao.java
+++ oculus/src/at/itb13/oculus/technicalServices/dao/EventtypeDao.java
@@ -1,8 +1,11 @@
 package at.itb13.oculus.technicalServices.dao;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
 
 import at.itb13.oculus.domain.Calendarevent;
@@ -10,36 +13,59 @@ import at.itb13.oculus.domain.Eventtype;
 import at.itb13.oculus.technicalServices.GenericDao;
 
 /**
- * TODO: Insert description here.
+ * This class is used to load and save {@link at.itb13.oculus.domain.Eventtype}s, 
+ * as well as load collections that have not been loaded from the database yet (in case of lazy loading).
  * 
  * @author Daniel Scheffknecht
  * @date 06.04.2015
  */
 public class EventtypeDao extends GenericDao<Eventtype> {
 	
+	private static final Logger _logger = LogManager.getLogger(EventtypeDao.class.getName());
+	
+	/**
+	 * @see GenericDao#GenericDao(Class);
+	 */
 	public EventtypeDao() {
 		super(Eventtype.class);
 	}
 	
+	/**
+	 * Loads the collection from the database into the entity.
+	 * No changes are made to the database.
+	 * <p>
+	 * Example:
+	 * 		loadCalendarevents(eventtype);
+	 * 		calendarevents = eventtype.getCalendarevents();
+	 * 
+	 * @param eventtype The Eventtype whose calendarevents should be loaded. It must not be in a transient state!
+	 * @return The Calendarevents that have been loaded. Returns {@link java.util.Collections#emptySet()} in case of failure.
+	 * @see GenericDao#loadCollection(T entity, Collection<?> collection)
+	 */
 	public Set<Calendarevent> loadCalendarevents(Eventtype eventtype) {
 		try {
 			loadCollection(eventtype, eventtype.getCalendarevents());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			_logger.error(e);
+			return Collections.emptySet();
 		}
 		
 		return eventtype.getCalendarevents();
 	}
-	// TODO: Add logging
-	
+
+	/**
+	 * Load the Eventtype with the specified name from the database.
+	 * 
+	 * @param name The name of the Eventtype that should be loaded from the database.
+	 * @return the desired Eventtype, or null, if no Eventtype with the supplied name was found.
+	 */
 	public Eventtype findByName(String name) {
 		Eventtype ev = null;
 		
-		List<Eventtype> eventtypes = findByCriteria(Restrictions.like("eventTypeName", name));
+		List<Eventtype> eventtypes = findByCriteria(Restrictions.eq("eventTypeName", name));
 		
 		if(eventtypes.size() > 1) {
-			// TODO logging warning, shouldn't happen.
+			_logger.warn("More than 1 Eventtype with the name'" + name + "' has been found!");
 		}
 
 		if(!eventtypes.isEmpty()) {

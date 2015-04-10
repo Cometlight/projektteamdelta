cd9dea7a53ef3fc4de540b6c6e20cc29850d9c71
FlorinMetzler <florinmetzler@hotmail.com>
2015-04-10 13:18:39 +0200

    PatientCreation

--- oculus/src/at/itb13/oculus/application/patient/PatientCreation.java
+++ oculus/src/at/itb13/oculus/application/patient/PatientCreation.java
@@ -14,8 +14,9 @@ import at.itb13.oculus.technicalServices.dao.PatientDao;
  */
 public class PatientCreation {
 	private Patient _p;
+	
 	public void createPatient(String doctor, String socialInsuranceNr, String firstName, String lastName, 
-							  Date birthday, String gender, String street, String postalCode, String city, 
+							  String birthday, String gender, String street, String postalCode, String city, 
 							  String countryIsoCode, String phone, String email){
 		_p = _p.buildPatient(doctor, socialInsuranceNr, firstName, lastName, birthday, gender, street, postalCode, city, countryIsoCode, phone, email);
 		PatientDao patientDao = new PatientDao();

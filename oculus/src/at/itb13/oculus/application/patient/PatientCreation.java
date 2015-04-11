8cab3e6c65e5faf19a2c976dc8a7b97ffbc2233b
Dragonaut1337 <andrew.sparr@students.fhv.at>
2015-04-11 16:40:46 +0200

    - Changed Patientcreation - deleted buildPatient - changed Constructor of Patient

--- oculus/src/at/itb13/oculus/application/patient/PatientCreation.java
+++ oculus/src/at/itb13/oculus/application/patient/PatientCreation.java
@@ -14,10 +14,16 @@ import at.itb13.oculus.technicalServices.dao.PatientDao;
  */
 public class PatientCreation {
 	private Patient _p;
-	public void createPatient(Doctor doctor, String socialInsuranceNr, String firstName, String lastName, 
-							  Date birthday, String gender, String street, String postalCode, String city, 
-							  String countryIsoCode, String phone, String email){
-		_p = _p.buildPatient(doctor, socialInsuranceNr, firstName, lastName, birthday, gender, street, postalCode, city, countryIsoCode, phone, email);
+
+	public void createPatient(Doctor doctor, String socialInsuranceNr,
+			String firstName, String lastName, Date birthday, String gender,
+			String street, String postalCode, String city,
+			String countryIsoCode, String phone, String email) {
+		
+		_p = new Patient(doctor, socialInsuranceNr, firstName, lastName,
+				birthday, gender, street, postalCode, city, countryIsoCode,
+				phone, email);
+		
 		PatientDao patientDao = new PatientDao();
 		patientDao.makePersistent(_p);
 	}

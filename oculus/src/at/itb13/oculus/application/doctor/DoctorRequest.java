3d392a028e4b5ee922dad9cf3987f2af4f1c1280
ktr2884 <karin.trommelschlaeger@students.fhv.at>
2015-04-13 10:16:53 +0200

    **

---
+++ oculus/src/at/itb13/oculus/application/doctor/DoctorRequest.java
@@ -0,0 +1,28 @@
+/**
+ * Provides a List of all doctors
+ * 
+ * @author Karin Trommelschläger
+ * @date 13.04.2015
+ * 
+ */
+package at.itb13.oculus.application.doctor;
+import java.util.ArrayList;
+import java.util.List;
+
+import at.itb13.oculus.domain.Doctor;
+import at.itb13.oculus.technicalServices.dao.DoctorDao;
+
+/**
+ * @author Karin
+ *
+ */
+public class DoctorRequest {
+
+	public List<Doctor> getdoctor(){
+		List<Doctor> doctorList = new ArrayList<Doctor>();
+		DoctorDao dd = new DoctorDao();
+		doctorList = dd.findAll();
+		return doctorList;
+		
+	}
+}

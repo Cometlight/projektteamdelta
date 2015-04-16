4d066632825b7844b0a9db45a7d4b7d4f610f6e7
Dragonaut1337 <andrew.sparr@students.fhv.at>
2015-04-16 23:19:17 +0200

    - fixed package errors (technicalservices/dao)

---
+++ oculus/src/at/itb13/oculustests/technicalservices/dao/DoctorDao_UnitTests.java
@@ -0,0 +1,14 @@
+package at.itb13.oculustests.technicalservices.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DoctorDao_UnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

88a98533ee16de17e37c0e40eacf0ca9eff7acde
Caroooo <caroline.meusburger@students.fhv.at>
2015-04-30 17:13:37 +0200

    calendar, first thoughts

--- oculus/src/at/itb13/oculus/presentation/view/TabCalendarController.java
+++ oculus/src/at/itb13/oculus/presentation/view/TabCalendarController.java
@@ -1,31 +1,17 @@
 package at.itb13.oculus.presentation.view;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import at.itb13.oculus.presentation.OculusMain;
 
-/**
- * TODO: Insert description here.
- * 
- * @author Caroline Meusburger
- * @since 30.04.2015
- */
-public class TabCalendarController {
 
-	@FXML
-	private WebView _webView;
+public class TabCalendarController{
+	
 	private OculusMain _main;
 	
-	//general Methods
 	 public void setMain(OculusMain main) {
 	        _main = main;
 	  }
-	 
 	 @FXML
-	 private void initialize() {
-		 final WebEngine engine = _webView.getEngine();
-		 engine.load("http://www.google.at/");
-	 }
-
+	 private void initialize() {}
 }

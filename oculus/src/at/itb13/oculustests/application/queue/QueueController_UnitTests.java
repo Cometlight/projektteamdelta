3a719f1a6a74f1c28b9645a7c8153e3d2b267579
Dragonaut1337 <andrew.sparr@students.fhv.at>
2015-05-03 20:47:42 +0200

    add #bestqueuecontroller_unittestsever laugh at #bestcommitmessageever

--- oculus/src/at/itb13/oculustests/application/queue/QueueController_UnitTests.java
+++ oculus/src/at/itb13/oculustests/application/queue/QueueController_UnitTests.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 
+import at.itb13.oculus.application.exceptions.InvalidInputException;
 import at.itb13.oculus.application.queue.QueueController;
 import at.itb13.oculus.domain.Queue;
+import at.itb13.oculus.domain.readonlyinterfaces.CalendarEventRO;
+import at.itb13.oculus.domain.readonlyinterfaces.PatientRO;
+import at.itb13.oculus.domain.readonlyinterfaces.QueueRO;
 
 /**
  * TODO: Insert description here.
@@ -27,13 +31,56 @@ public class QueueController_UnitTests {
 		assertEquals(true, qC != null);
 		assertEquals(true, qC.getQueue() == null);
 	}
-	
+
 	@Test
 	public void constructorWithMockedQueue() {
-		Queue q = Mockito.mock(Queue.class); 
+		Queue q = Mockito.mock(Queue.class);
 		QueueController qc = new QueueController(q);
 		assertEquals(true, qc != null);
 		assertEquals(true, qc.getQueue() != null);
+	}
+
+	@Test
+	public void pushQueueEntry() throws InvalidInputException {
+		// Queue q = Mockito.mock(Queue.class);
+		// QueueController qc = new QueueController(q);
+		QueueController qc = Mockito.mock(QueueController.class);
+		PatientRO pRO = Mockito.mock(PatientRO.class);
+		CalendarEventRO cRO = Mockito.mock(CalendarEventRO.class);
+		assertEquals(false, qc.pushQueueEntry(pRO, cRO));
+	}
+
+	@Test
+	public void popQueueEntry() {
+		QueueController qc = Mockito.mock(QueueController.class);
+		assertEquals(true, qc.popQueueEntry() == null);
+	}
+
+	@Test
+	public void peekQueueEntry() {
+		QueueController qc = Mockito.mock(QueueController.class);
+		assertEquals(true, qc.peekQueueEntry() == null);
+	}
+
+	@Test
+	public void removeQueueEntry() {
+		QueueController qc = Mockito.mock(QueueController.class);
+		PatientRO pRO = Mockito.mock(PatientRO.class);
+		assertEquals(false, qc.removeQueueEntry(pRO));
+	}
+
+	@Test
+	public void isPatientInQueue() {
+		QueueController qc = Mockito.mock(QueueController.class);
+		PatientRO pRO = Mockito.mock(PatientRO.class);
+		assertEquals(false, qc.isPatientInQueue(pRO));
+	}
+
+	@Test
+	public void representsSameQueueByID() {
+		QueueController qc = Mockito.mock(QueueController.class);
+		QueueRO pRO = Mockito.mock(QueueRO.class);
+		assertEquals(false, qc.representsSameQueueByID(pRO));
 	}
 
 }

b6f92a112694f08f575cc756f25845aa0bab118e
FlorinMetzler <florinmetzler@hotmail.com>
2015-05-03 20:07:00 +0200

    added the half methods of the NewAppointmentController

--- oculus/src/at/itb13/oculus/application/calendar/NewAppointmentController.java
+++ oculus/src/at/itb13/oculus/application/calendar/NewAppointmentController.java
@@ -1,22 +1,25 @@
-package at.itb13.oculus.application.interfaces;
+package at.itb13.oculus.application.calendar;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import at.itb13.oculus.application.ControllerFacade;
 import at.itb13.oculus.application.exceptions.InvalidInputException;
 import at.itb13.oculus.application.exceptions.SaveException;
+import at.itb13.oculus.application.interfaces.INewAppointmentController;
 import at.itb13.oculus.domain.interfaces.ICalendar;
 import at.itb13.oculus.domain.interfaces.ICalendarEvent;
 import at.itb13.oculus.domain.interfaces.IPatient;
 
 /**
- * This Interface defines the used methodes of the NewAppointmentController.
+ * TODO: Insert description here.
  *
  * @author Florin Metzler
- * @since 30.04.2015
+ * @since 03.05.2015
  */
-public interface INewAppointmentController {
-	
+public class NewAppointmentController implements INewAppointmentController {
+
 	/**
 	 * Creates a new appointment in a chosen timespan for the wanted calendar and patient.
 	 * 
@@ -27,10 +30,14 @@ public interface INewAppointmentController {
 	 * @param patient is the person who refers to the appointment.
 	 * @throws SaveException is throwen when an error occured while saving the new appointment.
 	 */
-	public abstract void newCalendarEvent(ICalendar calendar, LocalDateTime start, 
-										 LocalDateTime end, String description, 
-										 IPatient patient) throws SaveException;
-	
+	@Override
+	public void newCalendarEvent(ICalendar calendar, LocalDateTime start,
+			LocalDateTime end, String description, IPatient patient)
+			throws SaveException {
+		// TODO Auto-generated method stub
+		
+	}
+
 	/**
 	 * Creates a new appointment in a chosen timespan for the wanted calendar and patient.
 	 * 
@@ -41,10 +48,14 @@ public interface INewAppointmentController {
 	 * @param patient is the person who refers to the appointment.
 	 * @throws SaveException is throwen when an error occured while saving the new appointment.
 	 */
-	public abstract void newCalendarEvent(ICalendar calendar, LocalDateTime start, 
-			 							 LocalDateTime end, String description, 
-			 							 String patient) throws SaveException;
-	
+	@Override
+	public void newCalendarEvent(ICalendar calendar, LocalDateTime start,
+			LocalDateTime end, String description, String patient)
+			throws SaveException {
+		// TODO Auto-generated method stub
+		
+	}
+
 	/**
 	 * Creates a list of ICalendarEvent in a chosen timespan and for a chosen calendar.
 	 * 
@@ -52,16 +63,33 @@ public interface INewAppointmentController {
 	 * @param startDate the start Date of the timespan. (inclusive)
 	 * @param endDate the end Date of the timespan. (inclusive)
 	 * @return A list of ICalendarEvent.
-	 * @throws InvalidInputException throws when the end date is before the start date.
+	 * @throws InvalidInputException InvalidInputException throws when the end date is before the start date.
 	 */
-	public abstract List<ICalendarEvent> getCalendarEventsInTimespan(ICalendar calendar, 
-										 LocalDateTime start, LocalDateTime end) throws InvalidInputException;
-	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ICalendarEvent> getCalendarEventsInTimespan(ICalendar calendar,
+			LocalDateTime start, LocalDateTime end) throws InvalidInputException {
+		if(start.isBefore(end)){
+			return (List<ICalendarEvent>) calendar.getCalendarEventsForTimespan(start, end);			
+		}else{
+			throw new InvalidInputException();
+		}
+	}
+
 	/**
 	 * Creates a list of all ICalendar.
 	 * 
 	 * @return A list of all ICalendar.
 	 */
-	public abstract List<ICalendar> getAllCalendars();
+	@Override
+	public List<ICalendar> getAllCalendars() {
+		List<ICalendar> calendar = new ArrayList<>();
+		ControllerFacade facade = ControllerFacade.getInstance();
+		List<CalendarController> allController = facade.getAllCalendarController();
+		for(CalendarController controller : allController ){
+			calendar.add((ICalendar) controller.getCalendar());
+		}
+		return calendar;
+	}
 
 }

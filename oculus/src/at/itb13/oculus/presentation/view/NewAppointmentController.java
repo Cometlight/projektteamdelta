37a8ea00aadf0e6a4ee18a74223259508ae1a50f
Caroooo <caroline.meusburger@students.fhv.at>
2015-05-04 14:06:08 +0200

    New Appointment Dialog Time Spinner

--- oculus/src/at/itb13/oculus/presentation/view/NewAppointmentController.java
+++ oculus/src/at/itb13/oculus/presentation/view/NewAppointmentController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -66,26 +67,57 @@ public class NewAppointmentController {
 	 * fills the combobox doctor with calendars
 	 */
 	private void setItemsToDoctorBox() {
-		_doctorBox.setConverter(new CalendarSringConverter());
-		List<CalendarController> calContrs = ControllerFacade.getInstance().getAllCalendarController(); //--> TODO: Change to Interface INewAppointmentController getAllCalendars
-		for(CalendarController c : calContrs){
+//		_doctorBox.setConverter(new CalendarSringConverter());
+//		List<CalendarController> calContrs = ControllerFacade.getInstance().getAllCalendarController(); //--> TODO: Change to Interface INewAppointmentController getAllCalendars
+//		for(CalendarController c : calContrs){
 			//_doctorBox.getItems().add(c.getCalendar());
-		}
+//		}
 	}
 	
 	private void setItemsToTypeBox(){
-		_typeBox.setConverter(new CalendarEventTypeSringConverter());
+	//	_typeBox.setConverter(new CalendarEventTypeSringConverter());
 	//	_typeBox.getItems().addAll(ControllerFacade.getInstance().getINewAppointmentController.getAllEventType());
 	}
 	
 	private void initSpinner(){
-		SpinnerValueFactory<Integer> hours = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23);
-		_startTimeSpinnerHour.setValueFactory(hours);
-		_endTimeSpinnerHour.setValueFactory(hours);
 		
-		SpinnerValueFactory<Integer> mins = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59);
-		_startTimeSpinnerMin.setValueFactory(mins);
-		_endTimeSpinnerMin.setValueFactory(mins);
+		
+		SpinnerValueFactory<Integer> sHours = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23);
+		SpinnerValueFactory<Integer> sMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59);
+		SpinnerValueFactory<Integer> eHours = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23);
+		SpinnerValueFactory<Integer> eMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59);
+		
+		//start Hour
+		_startTimeSpinnerHour.setValueFactory(sHours);
+		SpinnerValueFactory.IntegerSpinnerValueFactory startHourFactory = (IntegerSpinnerValueFactory) _startTimeSpinnerHour.getValueFactory();  
+        startHourFactory.setMax(23); 
+        startHourFactory.setValue(0);
+        startHourFactory.setWrapAround(true);
+        _startTimeSpinnerHour.setEditable(true); 
+        
+        //End Hour
+        _endTimeSpinnerHour.setValueFactory(eHours);
+        SpinnerValueFactory.IntegerSpinnerValueFactory endHourFactory =(IntegerSpinnerValueFactory) _endTimeSpinnerHour.getValueFactory();
+        endHourFactory.setMax(23); 
+        endHourFactory.setValue(0);
+        endHourFactory.setWrapAround(true);
+        _endTimeSpinnerHour.setEditable(true);
+		
+		//start minutes
+        _startTimeSpinnerMin.setValueFactory(sMin);
+        SpinnerValueFactory.IntegerSpinnerValueFactory startMinFactory = (IntegerSpinnerValueFactory) _startTimeSpinnerMin.getValueFactory();
+        startMinFactory.setMax(59);
+        startMinFactory.setValue(0);
+        startMinFactory.setWrapAround(true);
+        _startTimeSpinnerMin.setEditable(true);
+        
+      //end minutes
+        _endTimeSpinnerMin.setValueFactory(eMin);
+        SpinnerValueFactory.IntegerSpinnerValueFactory endMinFactory = (IntegerSpinnerValueFactory) _endTimeSpinnerMin.getValueFactory();
+        endMinFactory.setMax(59);
+        endMinFactory.setValue(0);
+        endMinFactory.setWrapAround(true);
+        _endTimeSpinnerMin.setEditable(true);
 	}
 
 	/**

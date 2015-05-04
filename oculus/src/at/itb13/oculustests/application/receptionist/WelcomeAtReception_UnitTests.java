9acd1478663fb1535dddd8ce519979b3804338e4
ktr2884 <karin.trommelschlaeger@students.fhv.at>
2015-05-04 11:21:18 +0200

    Tests for PatientSearch added

---
+++ oculus/src/at/itb13/oculustests/application/receptionist/WelcomeAtReception_UnitTests.java
@@ -0,0 +1,17 @@
+package at.itb13.oculustests.application.receptionist;
+
+import org.junit.Test;
+
+import at.itb13.oculus.application.receptionist.WelcomeAtReception;
+
+/**
+ * TODO: Tests??
+ * 
+ * @author Karin Trommelschläger
+ * @date 04.05.2015
+ * 
+ */
+public class WelcomeAtReception_UnitTests {
+	
+	
+}

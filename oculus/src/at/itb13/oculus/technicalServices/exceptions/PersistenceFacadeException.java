acafe56ac3ef7e9b388862d05be9f2fdbfbb0a11
Andrew Sparr <andrew.sparr@students.fhv.at>
2015-05-11 13:23:55 +0200

    + add IPersistenceFacade, PersistenceFacade, PersistenceFacadeException, + add PersistenceFacade_UnitTests

---
+++ oculus/src/at/itb13/oculus/technicalServices/exceptions/PersistenceFacadeException.java
@@ -0,0 +1,11 @@
+package at.itb13.oculus.technicalServices.exceptions;
+
+/**
+ * TODO: Insert description here.
+ * 
+ * @author Andrew Sparr
+ * @date 11.05.2015
+ */
+public class PersistenceFacadeException {
+
+}

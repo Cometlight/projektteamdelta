6faf8cc6461846504c464d1657b31803788507e7
Caroooo <caroline.meusburger@students.fhv.at>
2015-05-12 11:06:35 +0200

    dayview

--- oculus/src/at/itb13/oculus/presentation/view/calendar/CalendarDayView.java
+++ oculus/src/at/itb13/oculus/presentation/view/calendar/CalendarDayView.java
@@ -24,10 +24,11 @@ public class CalendarDayView implements ICalendarViewState {
 		_gridPaneHeader.setGridLinesVisible(true); 	// TODO: "for debug purposes only" --> Durch CSS ersetzen
 		
 		_gridPaneHeader.add(new Text("Time"), 0, 0);
-		_gridPaneHeader.add(new Text(DayOfWeek.MONDAY.name()), 1, 0);
+		_gridPaneHeader.add(new Text(""), 1, 0);
 	}
 	@Override
 	public void changeHeader(LocalDate date){
+	
 		_gridPaneHeader.add(new Text("Time"), 0, 0);
 		_gridPaneHeader.add(new Text(date.getDayOfWeek().name() + " " + date.toString()), 1, 0);
 	}

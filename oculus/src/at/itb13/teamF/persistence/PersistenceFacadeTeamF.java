712ef270af51b30e402772890e67bc20cf9a4eee
Andrew Sparr <andrew.sparr@students.fhv.at>
2015-05-18 15:45:53 +0200

    add package 'persistence' add PersistenceFacadeTeamF

---
+++ oculus/src/at/itb13/teamF/persistence/PersistenceFacadeTeamF.java
@@ -0,0 +1,77 @@
+package at.itb13.teamF.persistence;
+
+/**
+ * TODO: Insert description here.
+ * 
+ * @author Andrew Sparr
+ * @date 18.05.2015
+ */
+
+import java.util.Collection;
+
+import at.oculus.teamf.databaseconnection.session.exception.ClassNotMappedException;
+import at.oculus.teamf.domain.entity.interfaces.IDomain;
+import at.oculus.teamf.persistence.IFacade;
+import at.oculus.teamf.persistence.exception.BadConnectionException;
+import at.oculus.teamf.persistence.exception.DatabaseOperationException;
+import at.oculus.teamf.persistence.exception.FacadeException;
+import at.oculus.teamf.persistence.exception.NoBrokerMappedException;
+import at.oculus.teamf.persistence.exception.search.InvalidSearchParameterException;
+import at.oculus.teamf.persistence.exception.search.SearchInterfaceNotImplementedException;
+
+public class PersistenceFacadeTeamF implements IFacade{
+
+	@Override
+	public <T> T getById(Class clazz, int id) throws BadConnectionException,
+			NoBrokerMappedException, DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> Collection<T> getAll(Class clazz) throws BadConnectionException,
+			NoBrokerMappedException, DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(IDomain obj) throws BadConnectionException,
+			NoBrokerMappedException, DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveAll(Collection<IDomain> obj)
+			throws BadConnectionException, NoBrokerMappedException,
+			DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(IDomain obj) throws BadConnectionException,
+			NoBrokerMappedException, InvalidSearchParameterException,
+			DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAll(Collection<IDomain> obj) throws FacadeException,
+			ClassNotMappedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <T> Collection<T> search(Class clazz, String... search)
+			throws SearchInterfaceNotImplementedException,
+			BadConnectionException, NoBrokerMappedException,
+			InvalidSearchParameterException, DatabaseOperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

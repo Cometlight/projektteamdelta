3dcd01d3196f0017a494d1cb9a6aa4a778935bf9
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-05-18 13:20:23 +0200

    Add first adapter

---
+++ oculus/src/at/itb13/teamF/interfaces/IAdapter.java
@@ -0,0 +1,11 @@
+package at.itb13.teamF.interfaces;
+
+/**
+ * TODO: Insert description here.
+ * 
+ * @author Daniel Scheffknecht
+ * @date May 18, 2015
+ */
+public interface IAdapter {
+	Object getDomainObject();
+}

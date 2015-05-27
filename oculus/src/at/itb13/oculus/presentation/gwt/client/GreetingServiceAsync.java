218a7174eabefcebfc1c47a03384c900dea3c448
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-05-27 14:28:23 +0200

    Add gwt test.

---
+++ oculus/src/at/itb13/oculus/presentation/gwt/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package at.itb13.oculus.presentation.gwt.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}

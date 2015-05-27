4d1d6200374a0ea23d790e26af5b1298c13f00c9
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-05-27 14:45:24 +0200

    Change to oculusweb

---
+++ oculus/src/at/itb13/oculus/application/patient/NewAppointment.java
@@ -0,0 +1,16 @@
+package at.itb13.oculus.application.patient;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * TODO: Insert description here.
+ * 
+ * @author Daniel Scheffknecht
+ * @date May 27, 2015
+ */
+public class NewAppointment {
+	private static final Logger _logger = LogManager.getLogger(NewAppointment.class.getName());
+	
+	
+}

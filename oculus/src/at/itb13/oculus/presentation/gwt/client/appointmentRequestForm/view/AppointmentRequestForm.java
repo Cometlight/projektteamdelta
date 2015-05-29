d509700df2e4134045e6473d716e7a72ed4b8e39
FlorinMetzler <florinmetzler@hotmail.com>
2015-05-29 12:36:41 +0200

    hallo daniel, commits, wir kriegen dich. ;)

--- oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentRequestForm/view/AppointmentRequestForm.java
+++ oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentRequestForm/view/AppointmentRequestForm.java
@@ -1,7 +1,12 @@
 package at.itb13.oculus.presentation.gwt.client.appointmentRequestForm.view;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -55,6 +60,12 @@ public class AppointmentRequestForm extends Composite {
 	@UiField
 	Label weekdayErrorLabel;
 	
+	@UiField
+	Label datepicker1ErrorLabel;
+	
+	@UiField
+	Label datepicker2ErrorLabel;
+	
 	@UiHandler("addButton")
 	void testButton(ClickEvent event) {
 		int index = weekdayListBox.getSelectedIndex();
@@ -66,4 +77,31 @@ public class AppointmentRequestForm extends Composite {
 		fromErrorLabel.setText("von: " + from);
 		toErrorLabel.setText("bis: " + to);
 	}
+	
+	@UiHandler("datepicker1")
+	void testDatepicker1(@SuppressWarnings("rawtypes") ValueChangeEvent event){
+		datepicker1.addValueChangeHandler(new ValueChangeHandler<Date>() {
+	         @Override
+	         public void onValueChange(ValueChangeEvent<Date> event) {
+	            Date date = event.getValue();
+	            String dateString = 
+	            DateTimeFormat.getFormat("MM/dd/yyyy").format(date);
+	            datepicker1ErrorLabel.setText(dateString);				
+	         }
+	      });
+	}
+	 
+	@UiHandler("datepicker2")
+	void testDatepicker2(@SuppressWarnings("rawtypes") ValueChangeEvent event){
+	 datepicker2.addValueChangeHandler(new ValueChangeHandler<Date>() {
+         @Override
+         public void onValueChange(ValueChangeEvent<Date> event) {
+            Date date = event.getValue();
+            String dateString = 
+            DateTimeFormat.getFormat("MM/dd/yyyy").format(date);
+            datepicker2ErrorLabel.setText(dateString);				
+         }
+      });
+	}
+
 }

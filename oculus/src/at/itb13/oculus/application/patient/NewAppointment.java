6d58db6a4672aec057f84dabbeaf3225034b3b61
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-06-01 10:39:27 +0200

    Add method to new appointment

--- oculus/src/at/itb13/oculus/application/patient/NewAppointment.java
+++ oculus/src/at/itb13/oculus/application/patient/NewAppointment.java
@@ -7,6 +7,8 @@ import java.util.Date;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import at.itb13.oculus.application.ControllerFacade;
+import at.itb13.oculus.domain.CalendarEvent;
 import at.itb13.oculus.domain.Patient;
 import at.itb13.oculus.technicalServices.dao.PatientDao;
 
@@ -22,7 +24,7 @@ public class NewAppointment {
 
 	/**
 	 * Checks if a patient with the given email exists, and if the password is
-	 * correct.
+	 * correct. In addition, it selects the patient in the ControllerFacade.
 	 * 
 	 * @param email
 	 *            the patient's email
@@ -38,10 +40,22 @@ public class NewAppointment {
 		Patient patient = PatientDao.getInstance().findByEmail(email);
 
 		if (patient != null) {
+			ControllerFacade.setPatientSelected(patient);
 			return patient.isEqualPassword(password);
 		}
 
 		return false;
+	}
+	
+	/**
+	 * In reference to the selected patient in the ControllerFacade.
+	 * 
+	 * @return true, if the selected patient has a future appointment.
+	 */
+	public Boolean hasFutureAppointment() {
+//		CalendarEvent calEv = ControllerFacade.getPatientSelected().getNextAppointment();
+//		return calEv != null;
+		return true;
 	}
 	
 	public Object getPossibleAppointment(String weekday, String from, String to, 

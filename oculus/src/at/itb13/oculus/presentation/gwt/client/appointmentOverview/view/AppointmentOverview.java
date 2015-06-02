d9901e172a1841a2c55cd0cad54c1fb597eaf9a3
ktr2884 <karin.trommelschlaeger@students.fhv.at>
2015-06-02 15:31:47 +0200

    Celltables added

--- oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentOverview/view/AppointmentOverview.java
+++ oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentOverview/view/AppointmentOverview.java
@@ -17,49 +17,72 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 public class AppointmentOverview extends Composite {
 
 	private int _calEventId;
-	private static AppointmentOverviewUiBinder uiBinder = GWT.create(AppointmentOverviewUiBinder.class);
+	private static AppointmentOverviewUiBinder uiBinder = GWT
+			.create(AppointmentOverviewUiBinder.class);
 	final AppointmentOverviewServiceAsync appointmentOverviewAsyncService = GWT
 			.create(AppointmentOverviewService.class);
 	private Patient _patient;
 
 	@UiTemplate("AppointmentOverview.ui.xml")
-	interface AppointmentOverviewUiBinder extends UiBinder<Widget, AppointmentOverview> {
+	interface AppointmentOverviewUiBinder extends
+			UiBinder<Widget, AppointmentOverview> {
 	}
 
 	@UiField(provided = true)
 	final AppointmentOverviewResources res;
-	
+	@UiField
+	Label nameLabel;
+	@UiField
+	Label sinLabel;
+	@UiField
+	Label doctorLabel;
+	// @UiField
+	// Label datecell;
+	// @UiField
+	// Label doctorcell;
+	// @UiField
+	// Label typecell;
+	// @UiField
+	// Label reasoncell;
+	@UiField
+	CellTable<CalendarEvent> existingAppointmentTable;
+	@UiField
+	VerticalPanel tablePanel;
+
+	@UiField
+	Button logoutButton;
+	@UiField
+	Button deleteButton;
+	@UiField
+	Button newAppointmentButton;
+	@UiField
+	Button appointmentChoiceButton;
+
 	public AppointmentOverview(Patient patient) {
 		this.res = GWT.create(AppointmentOverviewResources.class);
-		
+
 		res.style().ensureInjected();
 		initWidget(uiBinder.createAndBindUi(this));
 		_patient = patient;
-//		AsyncCallback<String[]> callbackPatient = new AsyncCallback<String[]>() {
-//
-//			@Override
-//			public void onFailure(Throwable caught) {
-//				Window.alert("AppointmentOverview - Failed to connect to server. Please try again in a few minutes, or contact the system administrator.");
-//			}
 
-//			@Override
-//			public void onSuccess(String[] result) {
-				nameLabel.setText(_patient.getName());
-				sinLabel.setText(_patient.getSin());
-				doctorLabel.setText(_patient.getDoctor());
-//			}
-//			
-//		};
+		nameLabel.setText(_patient.getName());
+		sinLabel.setText(_patient.getSin());
+		doctorLabel.setText(_patient.getDoctor());
+
 		AsyncCallback<CalendarEvent> callbackAppointment = new AsyncCallback<CalendarEvent>() {
 
 			@Override
@@ -70,105 +93,139 @@ public class AppointmentOverview extends Composite {
 			@Override
 			public void onSuccess(CalendarEvent cal) {
 				_calEventId = cal.getId();
-				datecell.setText(cal.getDate());
-				doctorcell.setText(cal.getDoctor());
-				typecell.setText(cal.getType());
-				reasoncell.setText(cal.getReason());
+
+				existingAppointmentTable = new CellTable<>();
+
+				TextColumn<CalendarEvent> DateColumn = new TextColumn<CalendarEvent>() {
+					@Override
+					public String getValue(CalendarEvent cal) {
+						return cal.getDate();
+					}
+				};
+
+				TextColumn<CalendarEvent> DoctorColumn = new TextColumn<CalendarEvent>() {
+					@Override
+					public String getValue(CalendarEvent cal) {
+						return cal.getDoctor();
+					}
+				};
+				TextColumn<CalendarEvent> TypeColumn = new TextColumn<CalendarEvent>() {
+					@Override
+					public String getValue(CalendarEvent cal) {
+						return cal.getType();
+					}
+				};
+				TextColumn<CalendarEvent> ReasonColumn = new TextColumn<CalendarEvent>() {
+					@Override
+					public String getValue(CalendarEvent cal) {
+						return cal.getReason();
+					}
+				};
+				existingAppointmentTable.addColumn(DateColumn, "Date");
+				// TODO: TIME
+				existingAppointmentTable.addColumn(DoctorColumn,
+						"Doctor / Orthoptis");
+				existingAppointmentTable.addColumn(TypeColumn,
+						"Appointment Type");
+				existingAppointmentTable.addColumn(ReasonColumn, "Reason");
+				// Create a data provider.
+				ListDataProvider<CalendarEvent> dataProvider = new ListDataProvider<CalendarEvent>();
+
+				// Connect the table to the data provider.
+				dataProvider.addDataDisplay(existingAppointmentTable);
+
+				// Add the data to the data provider, which automatically pushes
+				// it to the
+				// widget.
+				List<CalendarEvent> list = dataProvider.getList();
+
+				list.add(cal);
+				tablePanel.add(existingAppointmentTable);
+
+				// datecell.setText(cal.getDate());
+				// doctorcell.setText(cal.getDoctor());
+				// typecell.setText(cal.getType());
+				// reasoncell.setText(cal.getReason());
 				newAppointmentButton.setEnabled(false);
 			}
-			
+
 		};
-		
-		appointmentOverviewAsyncService.getPatientAppointment(_patient, callbackAppointment);
-		
+
+		appointmentOverviewAsyncService.getPatientAppointment(_patient,
+				callbackAppointment);
+
 	}
-	
-	@UiField
-	Label nameLabel;
-	@UiField
-	Label sinLabel;
-	@UiField
-	Label doctorLabel;
-	@UiField
-	Label datecell;
-	@UiField
-	Label doctorcell;
-	@UiField
-	Label typecell;
-	@UiField
-	Label reasoncell;
+
 	
 
-	@UiField
-	Button logoutButton;
-	@UiField
-	Button deleteButton;
-	@UiField
-	Button newAppointmentButton;
-	@UiField
-	Button appointmentChoiceButton;
-	
 	@UiHandler("newAppointmentButton")
 	void onClicknewAppointmentButton(ClickEvent event) {
-		
 
-//			AsyncCallback<Boolean> callback = new AsyncCallback<Boolean>() {
-//				@Override
-//				public void onFailure(Throwable caught) {
-//					Window.alert("Failed to connect to server. Please try again in a few minutes, or contact the system administrator.");
-//				}
-//
-//				@Override
-//				public void onSuccess(Boolean loginCredentialsValid) {
-//					if (loginCredentialsValid) {
-						Index.forward(new AppointmentRequestForm());
-//					} else {
-//						
-//					}
-//				}
-//				
-//			};
+		// AsyncCallback<Boolean> callback = new AsyncCallback<Boolean>() {
+		// @Override
+		// public void onFailure(Throwable caught) {
+		// Window.alert("Failed to connect to server. Please try again in a few minutes, or contact the system administrator.");
+		// }
+		//
+		// @Override
+		// public void onSuccess(Boolean loginCredentialsValid) {
+		// if (loginCredentialsValid) {
+		Index.forward(new AppointmentRequestForm());
+		// } else {
+		//
+		// }
+		// }
+		//
+		// };
 	}
-	
+
 	@UiHandler("deleteButton")
-	void onClickDeleteButton(ClickEvent event){
-		//delete CalendarEvent
-		
+	void onClickDeleteButton(ClickEvent event) {
+		// delete CalendarEvent
+
 		AsyncCallback<Boolean> callback = new AsyncCallback<Boolean>() {
-		@Override
-		public void onFailure(Throwable caught) {
-			Window.alert("DELETE- Failed to connect to server. Please try again in a few minutes, or contact the system administrator.");
-		}
-		
-		@Override
-		public void onSuccess(Boolean b) {
-			Window.alert("Event has been deleted");
-			datecell.setText("-");
-			doctorcell.setText("-");
-			typecell.setText("-");
-			reasoncell.setText("-");
-//			if (loginCredentialsValid) {
-//			//	Index.forward(new AppointmentRequestForm());
-//			} else {				
-//			}
-		}
-	};
-		
-		appointmentOverviewAsyncService.deleteAppointment(_calEventId, callback);
-		
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("DELETE- Failed to connect to server. Please try again in a few minutes, or contact the system administrator.");
+			}
+
+			@Override
+			public void onSuccess(Boolean b) {
+				Window.alert("Event has been deleted");
+				existingAppointmentTable.setVisible(false);
+				
+				
+				// datecell.setText("-");
+				// doctorcell.setText("-");
+				// typecell.setText("-");
+				// reasoncell.setText("-");
+				// if (loginCredentialsValid) {
+				// // Index.forward(new AppointmentRequestForm());
+				// } else {
+				// }
+			}
+		};
+
+		appointmentOverviewAsyncService
+				.deleteAppointment(_calEventId, callback);
+
 		deleteButton.setEnabled(false);
 		newAppointmentButton.setEnabled(true);
 	}
+
 	@UiHandler("appointmentChoiceButton")
 	void onClickAppointmentChoiceButton(ClickEvent event) {
-			CalendarEvent event1 = new CalendarEvent(25, "25.06.2015", "Dr.Tavolato", "First visit", "some reason");
-			CalendarEvent event2 = new CalendarEvent(26, "25.06.2015", "Dr Ruben", "First visit", "some reason");
-			CalendarEvent event3 = new CalendarEvent(27, "25.06.2015", "Dr Other", "First visit", "some reason");
-			List<CalendarEvent> events = new ArrayList<>();
-			events.add(event1);
-			events.add(event2);
-			events.add(event3);
-			Index.forward(new AppointmentChoice(_patient, events));
+		CalendarEvent event1 = new CalendarEvent(25, "25.06.2015",
+				"Dr.Tavolato", "First visit", "some reason");
+		CalendarEvent event2 = new CalendarEvent(26, "25.06.2015", "Dr Ruben",
+				"First visit", "some reason");
+		CalendarEvent event3 = new CalendarEvent(27, "25.06.2015", "Dr Other",
+				"First visit", "some reason");
+		List<CalendarEvent> events = new ArrayList<>();
+		events.add(event1);
+		events.add(event2);
+		events.add(event3);
+		Index.forward(new AppointmentChoice(_patient, events));
 
 	}
 }

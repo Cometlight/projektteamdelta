184dfa4e3647e778a3c135ed9547eefe3cb463d5
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-06-08 11:47:50 +0200

    fix appointmentchoice

--- oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentChoice/rpc/AppointmentChoiceService.java
+++ oculus/src/at/itb13/oculus/presentation/gwt/client/appointmentChoice/rpc/AppointmentChoiceService.java
@@ -1,10 +1,7 @@
 package at.itb13.oculus.presentation.gwt.client.appointmentChoice.rpc;
 
-import at.itb13.oculus.domain.CalendarEvent;
 import at.itb13.oculus.presentation.gwt.shared.Patient;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -15,7 +12,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
  * @date 29.05.2015
  * 
  */
-@RemoteServiceRelativePath("AppointmentOverviewService")
+@RemoteServiceRelativePath("AppointmentChoiceService")
 public interface AppointmentChoiceService extends RemoteService{
 	boolean addAppointment(Patient patient,
 			at.itb13.oculus.presentation.gwt.shared.CalendarEvent calendarEvent);
